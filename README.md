9510adbda46563a63c519fd132fc7ad92c7d6e37
Adrián Moreno <adrianmn90@gmail.com>
2024-04-06 19:57:13 +0200

    remove table before update auctions

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ![Auction Scrapper Diagram](/diagram/auction-scrapper-diagram.png)
 
+## Description
+
 This project is a scraper that extracts information about public auctions from the BOE auction portal.
 
 It's deployed to a lambda function that stores the auctions in a DynamoDB table.
+
+## Deployment
+
+1. `npm run build`
+2. Go to AWS Console > Lambda > auctionScrapper > Code > Upload from > .zip file
--- index.mjs
+++ index.mjs
@@ -1,25 +1,39 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DeleteTableCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
+const TABLE_NAME = "auction";
 
 export const handler = async (event) => {
-  // Obtener la primera subasta de próxima apertura
-  const response = addAuction({
-    id: "SUB-AT-2024-23R0886001470",
-    appraisal: 90607.17,
-    bidSpans: 1000,
-    deposit: 2998.76,
-    type: "agencia tributaria",
-    value: 59975.22,
-  });
+  await removeAllAuctions();
 
-  return response;
+  const auctions = getAuctions();
+
+  auctions.forEach(async (auction) => await addAuction(auction));
+
+  return;
 };
 
+async function removeAllAuctions() {
+  const command = new DeleteTableCommand({ TableName: TABLE_NAME });
+  return await client.send(command);
+}
+
+function getAuctions() {
+  return [
+    {
+      id: "SUB-AT-2024-23R0886001470",
+      appraisal: 90607.17,
+      bidSpans: 1000,
+      deposit: 2998.76,
+      type: "agencia tributaria",
+      value: 59975.22,
+    },
+  ];
+}
+
 async function addAuction(auctionInfo) {
-  const TABLE_NAME = "auction";
   const id = `${auctionInfo.id}-${new Date().getTime()}`;
   const auction = Object.assign({}, auctionInfo, { id });
   const command = new PutCommand({ TableName: TABLE_NAME, Item: auction });
