db00d03ddf017fbc42f1cb4523bcd0fe77c99aa0
Adrián Moreno <adrianmn90@gmail.com>
2024-04-08 09:03:56 +0200

    Wait table exist. First iteration

--- index.mjs
+++ index.mjs
@@ -1,23 +1,51 @@
-import { DeleteTableCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DeleteTableCommand,
+  CreateTableCommand,
+  DescribeTableCommand,
+  DynamoDBClient,
+} from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({ region: "eu-west-3" });
 const docClient = DynamoDBDocumentClient.from(client);
 const TABLE_NAME = "auction";
+const WAIT_TIME = 10;
 
-export const handler = async (event) => {
-  await removeAllAuctions();
+export async function handler(event) {
+  await deleteTable();
+  await wait(WAIT_TIME);
+
+  await createTable();
+  await waitTableExists();
 
   const auctions = getAuctions();
+  const response = await addAuctions(auctions);
+  console.log(response);
+  return response;
+}
 
-  auctions.forEach(async (auction) => await addAuction(auction));
+async function deleteTable() {
+  try {
+    const command = new DeleteTableCommand({ TableName: TABLE_NAME });
+    const response = await client.send(command);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-  return;
-};
+async function createTable() {
+  const command = new CreateTableCommand({
+    TableName: TABLE_NAME,
+    AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
+    KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
+    ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+  });
 
-async function removeAllAuctions() {
-  const command = new DeleteTableCommand({ TableName: TABLE_NAME });
-  return await client.send(command);
+  const response = await client.send(command);
+  console.log(response);
+  return response;
 }
 
 function getAuctions() {
@@ -33,9 +61,37 @@ function getAuctions() {
   ];
 }
 
+async function addAuctions(auctions) {
+  return Promise.all(auctions.map((auction) => addAuction(auction)));
+}
+
 async function addAuction(auctionInfo) {
   const id = `${auctionInfo.id}-${new Date().getTime()}`;
   const auction = Object.assign({}, auctionInfo, { id });
   const command = new PutCommand({ TableName: TABLE_NAME, Item: auction });
-  return await docClient.send(command);
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+}
+
+async function waitTableExists() {
+  let tableStatus;
+
+  while (tableStatus !== "ACTIVE") {
+    try {
+      const command = new DescribeTableCommand({ TableName: TABLE_NAME });
+      response = await client.send(command);
+      tableStatus = response?.Table?.TableStatus;
+    } catch (error) {
+      console.error("Error describing table:", error);
+    }
+  }
+}
+
+async function wait(seconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
 }
